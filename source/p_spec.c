0fe960f131438da7d2c5c578fa7d1ae2645cb9d1
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2023-08-11 22:05:44 +0200

    disable god mode when on sector type 11, by doing some bit twiddling instead of arithmetic

--- source/p_spec.c
+++ source/p_spec.c
@@ -10,6 +10,8 @@
  *  Jess Haas, Nicolas Kalkhof, Colin Phipps, Florian Schulze
  *  Copyright 2005, 2006 by
  *  Florian Schulze, Colin Phipps, Neil Stevens, Andrey Budko
+ *  Copyright 2023 by
+ *  Frenkel Smeijers
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
@@ -2171,7 +2173,7 @@ void P_PlayerInSpecialSector (player_t* player)
         break;
 
       case 11:
-        _g->player.cheats -= CF_GODMODE;
+        _g->player.cheats &= ~CF_GODMODE;
 
         if (!(_g->leveltime&0x1f))
           P_DamageMobj (player->mo, NULL, NULL, 20);
